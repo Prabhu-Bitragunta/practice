110e5ae1d1d08c8de0a125127f9ff7a895d24f7d
Anil Bitragunta <bitraguntapandu11@gmail.com>
2026-06-18 11:37:26 +0530

    first push

---
+++ CarInfo.java
@@ -0,0 +1,13 @@
+class CarInfo	{
+	public void main(String []args){
+		
+		String carName="EV";
+		int carNo=6482;
+		String brandName="maruthi";
+		System.out.println(carName);
+		System.out.println(carNo);
+		System.out.println(brandName);
+	}
+	
+}
+
